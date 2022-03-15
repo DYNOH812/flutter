8da6b0ad95ce36acd62753c47a842e9102863e46
Dynoh812 <munyuadennis@gmail.com>
2022-03-15 21:08:31 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_dynoh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_dynoh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DenoApp());
+}
+
+class DenoApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello dynoh'));
+  }
+}
